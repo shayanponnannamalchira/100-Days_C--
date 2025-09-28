e8d85f8334eb7941c928ca38ee26be4697523f66
shayanponnannamalchira <shayan.malchira@s.amity.edu>
2025-09-28 23:55:27 +0530

    DAY 1

---
+++ Print.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std; //
+
+int main()
+{
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout << "Hello World";
+    return 0;
+}
