12c1770b359bc90dffb33bb42906005f8adb7fd7
shayanponnannamalchira <shayan.malchira@s.amity.edu>
2025-10-02 10:07:39 +0530

    DAY 4

---
+++ DAY 4/ASCII.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter a character: ";
+    cin>>a;
+    cout<<"The ASCII value is "<<int(a);
+    return 0;
+}
